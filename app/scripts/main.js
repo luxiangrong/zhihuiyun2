fbf179dcfa3ed384895acfb1d24033a75017f9a0
LuXiangrong <luxiangrongwave@gmail.com>
2015-11-23 17:11:32 +0800

    完成

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,27 @@
 ;
-(function($){
-	$(document).ready(function(){
-		var sideNavItemHeight = $('.side-nav-wrap li').height();
-		$('.side-nav-wrap li').css('line-height', sideNavItemHeight + 'px');
+(function($) {
+    $(document).ready(function() {
+        var sideNavItemHeight = $('.side-nav-wrap li').height();
+        $('.side-nav-wrap li').css('line-height', sideNavItemHeight + 'px');
 
-		$('#btn-side-nav').on('click', function(e){
-			e.preventDefault();
-			$('.side-nav-wrap').toggleClass('active');
-		});
-	});
+        $('#btn-side-nav').on('click', function(e) {
+            e.preventDefault();
+            $('.side-nav-wrap').toggleClass('active');
+        });
+
+        $('input').iCheck({
+            checkboxClass: 'iradio_square-grey',
+            radioClass: 'iradio_square-grey',
+            increaseArea: '20%' // optional
+        });
+
+        $('.grid-item a').on('click', function(e){
+        	var _this = $(this);
+        	e.preventDefault();
+        	$(this).parent().addClass('flip');
+        	window.setTimeout(function(){
+        		window.location.href = $(_this).attr('href');
+        	}, 1000);
+        });
+    });
 })(jQuery);
