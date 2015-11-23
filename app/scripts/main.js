d31202746cc8c06abab397061f2a329c3bb1d289
LuXiangrong <luxiangrongwave@gmail.com>
2015-11-23 20:24:01 +0800

    我的收藏页面增加点击功能

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,5 +23,35 @@
         		window.location.href = $(_this).attr('href');
         	}, 1000);
         });
+
+
+        //我的收藏页面，点击编辑按钮
+        $('.title-bar .edit').on('click', function(e){
+            e.preventDefault();
+            $('.actions').toggleClass('hidden show');
+            $('.thumbnail.selectable a').on('click.preventDefault', function(e2){
+                e2.preventDefault();
+            });
+            $('.thumbnail.selectable').on('click.select', function(e){
+                e.preventDefault();
+                console.log(1);
+                $(this).toggleClass('selected');
+            });
+        });
+
+        $('.select-all').on('click', function(e){
+            e.preventDefault();
+            $('.selectable').addClass('selected');
+        });
+
+        $('.actions .cancle').on('click', function(e){
+            e.preventDefault();
+            $('.selectable').removeClass('selected');
+            $(this).closest('.actions').addClass('hidden').removeClass('show');
+            $('.thumbnail.selectable a').off('click.preventDefault');
+            $('.thumbnail.selectable').off('click.select');
+        });
+
+        
     });
 })(jQuery);
